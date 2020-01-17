7d36bd1b49c46c5e78b8abb2582c2c72cba1c5c5
ambegossi <andersonbegossi@gmail.com>
2020-01-16 23:03:57 -0300

    Added websockets

--- src/pages/List.js
+++ src/pages/List.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { View, StyleSheet, Image, AsyncStorage, ScrollView } from 'react-native';
+import socketio from 'socket.io-client';
+import { View, StyleSheet, Image, AsyncStorage, ScrollView, Alert, Text } from 'react-native';
 
 import SpotList from '../components/SpotList';
 
@@ -7,6 +8,18 @@ import logo from '../assets/logo.png'
 
 export default function List() {
     const [techs, setTechs] = useState([]);
+
+    useEffect(() => {
+        AsyncStorage.getItem('user').then(user_id => {
+            const socket = socketio('http://192.168.1.28:3333', {
+                query: { user_id }
+            })
+
+            socket.on('booking_response', booking => {
+                Alert.alert(`Sua reserva em ${booking.spot.company} em ${booking.date} foi ${booking.approved ? 'APROVADA' : 'REJEITADA'}`);
+            })
+        })
+    }, []);
 
     useEffect(() => {
         AsyncStorage.getItem('techs').then(storagedTechs => {
@@ -17,7 +30,7 @@ export default function List() {
     }, []);
 
     return (
-        <View style={StyleSheet.container}>
+        <View style={styles.container}>
             <Image style={styles.logo} source={logo} />
 
             <ScrollView>
@@ -30,13 +43,14 @@ export default function List() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        paddingVertical: 10,
     },
 
     logo: {
         height: 32,
         resizeMode: 'contain',
         alignSelf: 'center',
-        marginTop: 50
+        marginTop: 30
     },
 
 });
